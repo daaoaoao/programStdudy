0d0237eac9b5a82edfc82be36f8a235e71f915fb
smartdoublej <1535086745@qq.com>
2022-05-23 14:47:20 +0800

    python

---
+++ python/案例/jpgtoPNG.py
@@ -0,0 +1,15 @@
+# 图片格式转换, Jpg转Png
+
+# 方法①
+from PIL import Image
+
+img = Image.open('test.jpg')
+img.save('test1.png')
+
+
+# 方法②
+
+# from cv2 import imread, imwrite
+
+# image = imread("test.jpg", 1)
+# imwrite("test2.png", image)
